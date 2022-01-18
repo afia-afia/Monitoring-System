51a69bd1b2258601512628068c41e837050f0e56
afia-afia <nabil.afia.afia@gmail.com>
2022-01-18 09:28:33 +0100

    inject dipendency iinto the collector using factory class

---
+++ monitoring-system/information-collector/src/main/java/org/emp/it/information/collector/TimerService.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package org.emp.it.information.collector;
+
+import java.util.Date;
+
+/**
+ *
+ * @author nabil
+ */
+public interface TimerService {
+    public Date getTimer(String ipAddres , String oid );
+    
+}
