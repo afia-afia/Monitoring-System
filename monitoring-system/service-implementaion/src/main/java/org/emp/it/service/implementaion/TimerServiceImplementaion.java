7798c139e4263ff8c157abd95a7e6f09d0d4458c
afia-afia <nabil.afia.afia@gmail.com>
2022-01-18 14:44:19 +0100

    sending procedure

--- monitoring-system/service-implementaion/src/main/java/org/emp/it/service/implementaion/TimerServiceImplementaion.java
+++ monitoring-system/service-implementaion/src/main/java/org/emp/it/service/implementaion/TimerServiceImplementaion.java
@@ -4,10 +4,22 @@
  */
 package org.emp.it.service.implementaion;
 
+import java.util.Date;
+import org.emp.it.information.collector.TimerService;
+
 /**
  *
  * @author nabil
+ * this class will interact directly with snmp
  */
-public class TimerServiceImplementaion {
+public class TimerServiceImplementaion implements  TimerService{
+
+    public Date getTimer(String ipAddres, String oid) {
+       
+        throw new UnsupportedOperationException("Not supported yet."); 
+//To change body of generated methods, choose Tools | Templates.
+
+
+    }
     
 }
